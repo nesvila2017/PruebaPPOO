244785a7b3f15575f96dca7649297814546818c9
joseomar-acer <joseomar-acer@TOSHIBA>
2017-04-01 11:04:59 -0500

    Java App PPOO Primera Subida

---
+++ PruebaPPOO/src/pruebappoo/PruebaPPOO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebappoo;
+
+/**
+ *
+ * @author joseomar-acer
+ */
+public class PruebaPPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hola Profundizacion Orientada a Objetos");
+    }
+    
+}
